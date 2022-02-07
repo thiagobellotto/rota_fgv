4faf0f913b2d252ec48c8bf69fed244f59258f33
Thiago Bellotto <thiago.br@live.com>
2022-02-07 20:50:22 -0300

    Add files via upload

--- streamlit.py
+++ streamlit.py
@@ -3,9 +3,7 @@ st.set_page_config(page_title="Rota 2030 - FGV",
     page_icon="https://logodownload.org/wp-content/uploads/2016/09/fgv-logo-0.png",
     layout="centered")
 
-st.title("Rota 2030 - FGV")
-
-frame = '''<iframe title="Rota_2030_v3" width="1024" height="612" src="https://app.powerbi.com/view?r=eyJrIjoiMDBkYjIxNDktZjU0My00NTdiLTk4MTEtMjY3M2Q2YWU0MGZmIiwidCI6ImQ2MzljZTdiLTBiNjUtNGJmOS1iNTk2LTllYzI2OWM1OTYxOSJ9&pageName=ReportSection" frameborder="0" allowFullScreen="true"></iframe>'''
+st.title("Rota 2030 - FGV - Fase 2")
 
 footer="""<style>
 a:link , a:visited{
@@ -39,4 +37,14 @@ footer {visibility: hidden;}
 </style> """, unsafe_allow_html=True)
 
 # display an embeddded from power bi
-st.markdown(frame, unsafe_allow_html=True)
+st.subheader("Matriz Importância X Desempenho")
+grafico_1 = '''<iframe title="fase2- grafico1" width="1024" height="612" src="https://app.powerbi.com/view?r=eyJrIjoiY2Y2NGNlMjQtZDA1OS00MDUzLWEzNTYtMzE3NGQzNjg4NzZiIiwidCI6ImQ2MzljZTdiLTBiNjUtNGJmOS1iNTk2LTllYzI2OWM1OTYxOSJ9&pageName=ReportSection4002c908007b02add658" frameborder="0" allowFullScreen="true"></iframe>'''
+st.markdown(grafico_1, unsafe_allow_html=True)
+
+st.subheader("Desempenho em todos os atributos por ferramentaria")
+grafico_2 = '''<iframe title="fase2-grafico2" width="1024" height="612" src="https://app.powerbi.com/view?r=eyJrIjoiOGFiMzgyOGUtNDYxOC00YzRiLTk2OGQtZWJmZGM0ZWI2MjY2IiwidCI6ImQ2MzljZTdiLTBiNjUtNGJmOS1iNTk2LTllYzI2OWM1OTYxOSJ9&pageName=ReportSectiona39ac7872978bc775d4c" frameborder="0" allowFullScreen="true"></iframe>'''
+st.markdown(grafico_2, unsafe_allow_html=True)
+
+st.subheader("Desempenho em todos as ferramentarias por atributo")
+grafico_3 = '''<iframe title="fase2-grafico3" width="1024" height="612" src="https://app.powerbi.com/view?r=eyJrIjoiMjJjOGNlNDEtMzlhNS00YzQ2LWI4ZjYtN2I5ZmFmY2Q5ZDY4IiwidCI6ImQ2MzljZTdiLTBiNjUtNGJmOS1iNTk2LTllYzI2OWM1OTYxOSJ9&pageName=ReportSection57869b2859ac5e89e618" frameborder="0" allowFullScreen="true"></iframe>'''
+st.markdown(grafico_3, unsafe_allow_html=True)
