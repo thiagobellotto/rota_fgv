7a4202a7e20fe39f5d5f352413d0965ea995d90a
Thiago Bellotto <thiago.br@live.com>
2022-02-08 23:02:41 -0300

    Add files via upload

--- streamlit.py
+++ streamlit.py
@@ -3,7 +3,7 @@ st.set_page_config(page_title="Rota 2030 - FGV",
     page_icon="https://logodownload.org/wp-content/uploads/2016/09/fgv-logo-0.png",
     layout="centered")
 
-st.title("Rota 2030 - FGV - Fase 2 - Versão 2")
+st.title("Rota 2030 - FGV - Fase 2")
 
 footer="""<style>
 a:link , a:visited{
@@ -30,6 +30,7 @@ text-align: center;
 <p>Developed by <a style='display: block; color: black; text-align: center;' href="https://www.linkedin.com/in/thiago-bellotto/" target="_blank">Thiago Bellotto</a></p>
 </div>
 """
+
 st.markdown(footer, unsafe_allow_html=True)
 st.markdown(""" <style>
 #MainMenu {visibility: hidden;}
@@ -38,28 +39,13 @@ footer {visibility: hidden;}
 
 # display an embeddded from power bi
 st.subheader("Matriz Importância X Desempenho")
-grafico_1 = '''<iframe title="fase2- grafico1" width="1024" height="612" src="https://app.powerbi.com/view?r=eyJrIjoiY2Y2NGNlMjQtZDA1OS00MDUzLWEzNTYtMzE3NGQzNjg4NzZiIiwidCI6ImQ2MzljZTdiLTBiNjUtNGJmOS1iNTk2LTllYzI2OWM1OTYxOSJ9&pageName=ReportSection4002c908007b02add658" frameborder="0" allowFullScreen="true"></iframe>'''
+grafico_1 = '''<iframe title="fase2- grafico1-final" width="1024" height="612" src="https://app.powerbi.com/view?r=eyJrIjoiYTU5YWE4ZTAtMDY0MS00Njc4LTgxYTYtNjEwMjA3ZGFhYzc4IiwidCI6ImQ2MzljZTdiLTBiNjUtNGJmOS1iNTk2LTllYzI2OWM1OTYxOSJ9&pageName=ReportSection4002c908007b02add658" frameborder="0" allowFullScreen="true"></iframe>'''
 st.markdown(grafico_1, unsafe_allow_html=True)
 
 st.subheader("Desempenho em todos os atributos por ferramentaria")
-grafico_2 = '''<iframe title="fase2-grafico2" width="1024" height="612" src="https://app.powerbi.com/view?r=eyJrIjoiOGFiMzgyOGUtNDYxOC00YzRiLTk2OGQtZWJmZGM0ZWI2MjY2IiwidCI6ImQ2MzljZTdiLTBiNjUtNGJmOS1iNTk2LTllYzI2OWM1OTYxOSJ9&pageName=ReportSectiona39ac7872978bc775d4c" frameborder="0" allowFullScreen="true"></iframe>'''
+grafico_2 = '''<iframe title="fase2-grafico2-final" width="1024" height="612" src="https://app.powerbi.com/view?r=eyJrIjoiMmRmNTZkYWUtMzM5MC00YTc3LTliZTItY2I0ZTJlODYxM2M2IiwidCI6ImQ2MzljZTdiLTBiNjUtNGJmOS1iNTk2LTllYzI2OWM1OTYxOSJ9&pageName=ReportSectiona39ac7872978bc775d4c" frameborder="0" allowFullScreen="true"></iframe>'''
 st.markdown(grafico_2, unsafe_allow_html=True)
 
 st.subheader("Desempenho em todos as ferramentarias por atributo")
-grafico_3 = '''<iframe title="fase2-grafico3" width="1024" height="612" src="https://app.powerbi.com/view?r=eyJrIjoiMjJjOGNlNDEtMzlhNS00YzQ2LWI4ZjYtN2I5ZmFmY2Q5ZDY4IiwidCI6ImQ2MzljZTdiLTBiNjUtNGJmOS1iNTk2LTllYzI2OWM1OTYxOSJ9&pageName=ReportSection57869b2859ac5e89e618" frameborder="0" allowFullScreen="true"></iframe>'''
+grafico_3 = '''<iframe title="fase2-grafico3-final" width="1024" height="612" src="https://app.powerbi.com/view?r=eyJrIjoiZGM0ZTE3Y2MtN2JmZS00Y2ZkLTk5MjMtMzY2YzljOWU5YjQzIiwidCI6ImQ2MzljZTdiLTBiNjUtNGJmOS1iNTk2LTllYzI2OWM1OTYxOSJ9&pageName=ReportSection57869b2859ac5e89e618" frameborder="0" allowFullScreen="true"></iframe>'''
 st.markdown(grafico_3, unsafe_allow_html=True)
-
-
-st.title("Rota 2030 - FGV - Fase 2 - Versão 1")
-
-st.subheader("Matriz Importância X Desempenho")
-grafico_4 = '''<iframe title="fase2- resposta principal - FASE 2" width="1024" height="612" src="https://app.powerbi.com/view?r=eyJrIjoiOTdjMTM5YTAtNjgwMS00OTAyLWJiYmEtYTgzZDQ0NjdiOGZmIiwidCI6ImQ2MzljZTdiLTBiNjUtNGJmOS1iNTk2LTllYzI2OWM1OTYxOSJ9" frameborder="0" allowFullScreen="true"></iframe>'''
-st.markdown(grafico_4, unsafe_allow_html=True)
-
-st.subheader("Desempenho em todos os atributos por ferramentaria")
-grafico_5 = '''<iframe title="fase2-grafico2-KENYTH - Respostas por Ferramentaria" width="1024" height="612" src="https://app.powerbi.com/view?r=eyJrIjoiYmFhY2RlZTMtNTFjYy00ODAwLWI5ZTYtM2QxNTQxZjg2ZDkzIiwidCI6ImQ2MzljZTdiLTBiNjUtNGJmOS1iNTk2LTllYzI2OWM1OTYxOSJ9" frameborder="0" allowFullScreen="true"></iframe>'''
-st.markdown(grafico_5, unsafe_allow_html=True)
-
-st.subheader("Desempenho em todos as ferramentarias por atributo")
-grafico_6 = '''<iframe title="fase2-grafico3-KENYTH - Respostas por Atributo" width="1024" height="612" src="https://app.powerbi.com/view?r=eyJrIjoiNjNlNjlkZTgtYjU4Ni00M2VmLWI0NTUtOGEyYTk4OTE0Y2IwIiwidCI6ImQ2MzljZTdiLTBiNjUtNGJmOS1iNTk2LTllYzI2OWM1OTYxOSJ9" frameborder="0" allowFullScreen="true"></iframe>'''
-st.markdown(grafico_6, unsafe_allow_html=True)
